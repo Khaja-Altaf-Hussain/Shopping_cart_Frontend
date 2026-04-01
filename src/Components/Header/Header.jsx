5014ead5e4d9bfd9270c68ca175700a417524984
Khaja Altaf Hussain <khajaaltafhussain47@gmail.com>
2026-04-01 21:21:25 +0530

    fix images for vercel

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { selectIsAuthenticated } from '../../store/authSlice'
 import {  useSelector } from 'react-redux'
-
+import logo from "../assets/Logo.png"
+import cartlogo from "../assets/OIP.webp"
 
 function Header() {
   const [open,setOpen]=useState(false)
@@ -19,12 +20,12 @@ function Header() {
         <nav className='border-gray-700 px-4 lg:px-6 py-2.5'>
           <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen'>
             <Link to="/" className=' flex items-center'>
-              <img src="\src\assets\Logo.png" alt="Logo" className=' scale-170 indexmr-3 h-12' />
+              <img src={logo} alt="Logo" className=' scale-170 indexmr-3 h-12' />
             </Link>
             <div className=' flex items-center lg:order-2'>
               {isAuthenticated && (<Link to="/cart" 
               className=' text-amber-200 hover:text-amber-300 hover:bg-white/20 mix-blend-darken active:scale-90 focus:ring-indigo-700 font-medium rounded-lg  text-sm  px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none' >
-                <img src="\src\assets\OIP.webp" alt="CART" className='h-12 mix-blend-multiply' />
+                <img src={cartlogo} alt="CART" className='h-12 mix-blend-multiply' />
               </Link>)}
               {!isAuthenticated?(<Link to="/users/login" 
               className=' text-amber-200 hover:text-amber-300 hover:bg-white/20 bg-white/10 active:scale-90 focus:ring-indigo-700 font-medium rounded-lg  text-sm  px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none'>
